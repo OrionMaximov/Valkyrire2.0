4edb207e8e0ddf9afb05642564c775896cf2a888
OrionMaximov <quentin.nicodeme@gmail.com>
2023-01-12 12:14:40 +0100

    modif des img et ajustement des form

--- src/Form/AddToCartType.php
+++ src/Form/AddToCartType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\OrderItem;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,7 +13,9 @@ class AddToCartType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('quantite');
+        $builder->add('quantite',NumberType::class,[
+            
+        ]);
         $builder->add('ajouter', SubmitType::class, [
             'label' => 'Ajouter au panier'
         ]);
