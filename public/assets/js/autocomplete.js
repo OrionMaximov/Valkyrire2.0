e326b8120d110b11398923eee58222aa7e1609ea
OrionMaximov <quentin.nicodeme@gmail.com>
2023-01-19 12:36:49 +0100

    enfin des truc qui fonctionne bien

---
+++ public/assets/js/autocomplete.js
@@ -0,0 +1,28 @@
+  jQuery(document).ready(function($) {
+    $("#searchBar").submit(function(e) {
+        e.preventDefault();
+        var query = $(".form-control").val();
+        $.ajax({
+            type: "POST",
+            url: "/search",
+            data: { query: query },
+            success: function(response) {
+                let books = response;
+                let results = $("#results");
+                results.html("");
+                $("#results").insertAfter("#searchBar");
+                for (var i = 0; i < books.length; i++) {
+                    var book = books[i];
+                    results.append("<p class='book-result' data-id='"+ book.id + "'>" + book.titre + " par " + book.auteur + "</p>");
+                } 
+                $(".book-result").on("click", function() {
+                    var bookId = $(this).data("id");
+                    window.location.href = "/livres/" + bookId;
+                });
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Livres;
 use App\Repository\LivresRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -47,5 +50,17 @@ class HomeController extends AbstractController
         return $this->render('home/ageverif.html.twig', [
             
         ]);
+    }
+
+    /**
+     * @Route("/search", name="searchBooks",methods={"POST"})
+     */
+    public function searchBooks(Request $request,ManagerRegistry $doctrine)
+    {
+        $query = $request->request->get('query');
+        $livre=$doctrine->getRepository(Livres::class);
+        $books = $livre->searchBooks($query);
+        $books = array_slice($books, 0, 15);
+        return $this->json($books);
     }
 }
